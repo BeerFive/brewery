3d010b74514432feeb4aa520e340938c1d64b1fe
Denis Rubel <denys.rubel@nure.ua>
2022-07-08 20:01:15 +0300

    Set authorization

--- src/main/java/ua/beerfive/brewery/controller/handler/AccessHandler.java
+++ src/main/java/ua/beerfive/brewery/controller/handler/AccessHandler.java
@@ -1,15 +1,17 @@
 package ua.beerfive.brewery.controller.handler;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 public class AccessHandler {
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/", "/home").permitAll()
-//                .antMatchers("/hello").hasRole("USER")
-//                .and()
-//                .formLogin().permitAll()
-//                .defaultSuccessUrl("/products")
-//                .and()
-//                .logout().permitAll();
-//    }
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/products").permitAll()
+                .antMatchers("/profile").hasRole("USER")
+                .and()
+                .formLogin().permitAll()
+                .defaultSuccessUrl("/products")
+                .and()
+                .logout().permitAll();
+    }
 }
